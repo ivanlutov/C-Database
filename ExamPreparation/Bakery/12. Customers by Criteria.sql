eacaa0bddf637861b9d7b2b2d6b8aa4f4917e151
ivanlutov <ivanlutov@abv.bg>
2017-10-20 03:18:54 +0300

    Update 12. Customers by Criteria.sql

--- ExamPreparation/Bakery/12. Customers by Criteria.sql
+++ ExamPreparation/Bakery/12. Customers by Criteria.sql
@@ -4,5 +4,5 @@ INNER JOIN Countries AS co
         ON co.Id = c.CountryId
      WHERE (c.Age >= 21 AND c.FirstName LIKE '%an%') 
             OR
-	       (c.PhoneNumber LIKE '%38' AND co.Name <> 'Greece')
+	   (c.PhoneNumber LIKE '%38' AND co.Name <> 'Greece')
   ORDER BY c.FirstName, c.Age DESC
