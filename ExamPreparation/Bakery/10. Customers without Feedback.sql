fd0c765df9d6e1565ed25eb30c4ea95b9e74d0ea
ivanlutov <ivanlutov@abv.bg>
2017-10-20 03:17:46 +0300

    Update 10. Customers without Feedback.sql

--- ExamPreparation/Bakery/10. Customers without Feedback.sql
+++ ExamPreparation/Bakery/10. Customers without Feedback.sql
@@ -1,6 +1,6 @@
    SELECT CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
           c.PhoneNumber,
-		  c.Gender
+	  c.Gender
      FROM Customers AS c
 LEFT JOIN Feedbacks AS f
        ON f.CustomerId = c.Id
