3afbd196145ae0cb84bc30f7a8970ec678d98bd8
ivanlutov <ivanlutov@abv.bg>
2017-10-20 03:20:51 +0300

    Update 16. Customers With Countries.sql

--- ExamPreparation/Bakery/16. Customers With Countries.sql
+++ ExamPreparation/Bakery/16. Customers With Countries.sql
@@ -2,8 +2,8 @@ CREATE VIEW v_UserWithCountries
          AS
      SELECT CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
             c.Age,
-	        c.Gender,
-	        co.Name AS CountryName
+	    c.Gender,
+	    co.Name AS CountryName
        FROM Customers AS c
  INNER JOIN Countries AS co
          ON co.Id = c.CountryId
